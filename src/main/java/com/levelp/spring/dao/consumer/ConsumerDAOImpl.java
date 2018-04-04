d0f0fbe5c4dde83e944b98758bf041a188071690
Salim <salimsa1@mail.ru>
2018-04-04 20:00:08 +0300

    second commit

--- src/main/java/com/levelp/spring/dao/consumer/ConsumerDAOImpl.java
+++ src/main/java/com/levelp/spring/dao/consumer/ConsumerDAOImpl.java
@@ -5,10 +5,11 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Component
+@Repository
 public class ConsumerDAOImpl implements ConsumerDAO {
 
     private final SessionFactory factory;
